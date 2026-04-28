1a79a94edab78cfc3cf63bdd07a4afbe6d4cfe04
Florin <andreinastaseandu277@gmail.com>
2026-04-28 14:23:51 +0000

    Singleton + Autentificare + STL (map, set)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <set>
 #include <string>
-#include <stdexcept>
 #include <memory>
+#include <stdexcept>
 
 using namespace std;
 
@@ -28,11 +30,11 @@ class Account
 {
 protected:
     double balance = 0;
-    vector<string> history; // STL: vector
+    vector<string> history;
 
 public:
     virtual ~Account() = default;
-    virtual void withdraw(double a) = 0; // functie pur virtuala
+    virtual void withdraw(double a) = 0;
     virtual string type() const = 0;
 
     void deposit(double a)
@@ -53,7 +55,6 @@ public:
     }
 };
 
-// Mostenire: Savings
 class Savings : public Account
 {
 public:
@@ -67,7 +68,6 @@ public:
     string type() const override { return "Economii"; }
 };
 
-// Mostenire: Checking
 class Checking : public Account
 {
 public:
@@ -81,33 +81,158 @@ public:
     string type() const override { return "Curent"; }
 };
 
-// Main
+// Singleton + STL (map, set)
+class Bank
+{
+    map<string, string> users;              // username -> parola
+    map<int, unique_ptr<Account>> accounts; // ID -> cont
+    set<int> ids;                           // ID-uri unice
+    int nextId = 1;
+
+    Bank() {} // constructor privat
+
+public:
+    static Bank &get()
+    {
+        static Bank b;
+        return b;
+    }
+
+    void reg(const string &u, const string &p)
+    {
+        if (users.count(u))
+            throw BankErr("Utilizator deja existent!");
+        users[u] = p;
+    }
+
+    void login(const string &u, const string &p)
+    {
+        if (!users.count(u) || users[u] != p)
+            throw BankErr("Date incorecte!");
+    }
+
+    int openAcc(int tip)
+    {
+        unique_ptr<Account> acc = (tip == 1)
+                                      ? static_cast<unique_ptr<Account>>(make_unique<Savings>())
+                                      : static_cast<unique_ptr<Account>>(make_unique<Checking>());
+        int id = nextId++;
+        accounts[id] = move(acc);
+        ids.insert(id);
+        return id;
+    }
+
+    Account *getAcc(int id)
+    {
+        if (!ids.count(id))
+            throw BankErr("Cont inexistent!");
+        return accounts[id].get();
+    }
+
+    void transfer(int from, int to, double amt)
+    {
+        getAcc(from)->withdraw(amt);
+        getAcc(to)->deposit(amt);
+    }
+};
+
+// Meniu
 int main()
 {
-    try
+    auto &bank = Bank::get();
+    string loggedInUser;
+    int opt;
+
+    while (true)
     {
-        unique_ptr<Account> acc1 = make_unique<Savings>();
-        unique_ptr<Account> acc2 = make_unique<Checking>();
+        cout << "\n=== SIMULATOR BANCAR ===\n"
+             << "1. Register\n2. Login\n3. Open Economii\n"
+             << "4. Open Curent\n5. Deposit\n6. Withdraw\n"
+             << "7. Transfer\n8. Sold\n0. Exit\n> ";
+        cin >> opt;
 
-        acc1->deposit(1000);
-        acc1->withdraw(200);
+        try
+        {
+            if (opt == 0)
+                break;
 
-        acc2->deposit(500);
-        acc2->withdraw(800);
-
-        cout << "=== Cont 1 ===\n";
-        acc1->printHistory();
-
-        cout << "\n=== Cont 2 ===\n";
-        acc2->printHistory();
-
-        // Test eroare
-        cout << "\n--- Test eroare ---\n";
-        acc1->withdraw(9999); // arunca exceptie
-    }
-    catch (BankErr &e)
-    {
-        cout << "[EROARE] " << e.what() << endl;
+            if (opt == 1)
+            {
+                string u, p;
+                cout << "Username: ";
+                cin >> u;
+                cout << "Parola: ";
+                cin >> p;
+                bank.reg(u, p);
+                loggedInUser = u;
+                cout << "Inregistrat si autentificat!\n";
+            }
+            else if (opt == 2)
+            {
+                string u, p;
+                cout << "Username: ";
+                cin >> u;
+                cout << "Parola: ";
+                cin >> p;
+                bank.login(u, p);
+                loggedInUser = u;
+                cout << "Autentificare reusita!\n";
+            }
+            else if (opt == 3)
+            {
+                cout << "Cont Economii creat, ID: " << bank.openAcc(1) << "\n";
+            }
+            else if (opt == 4)
+            {
+                cout << "Cont Curent creat, ID: " << bank.openAcc(2) << "\n";
+            }
+            else if (opt == 5)
+            {
+                int id;
+                double s;
+                cout << "ID cont: ";
+                cin >> id;
+                cout << "Suma: ";
+                cin >> s;
+                bank.getAcc(id)->deposit(s);
+                cout << "Sold: " << formatMoney(bank.getAcc(id)->getBalance()) << "\n";
+            }
+            else if (opt == 6)
+            {
+                int id;
+                double s;
+                cout << "ID cont: ";
+                cin >> id;
+                cout << "Suma: ";
+                cin >> s;
+                bank.getAcc(id)->withdraw(s);
+                cout << "Sold: " << formatMoney(bank.getAcc(id)->getBalance()) << "\n";
+            }
+            else if (opt == 7)
+            {
+                int f, t;
+                double s;
+                cout << "De la ID: ";
+                cin >> f;
+                cout << "Catre ID: ";
+                cin >> t;
+                cout << "Suma: ";
+                cin >> s;
+                bank.transfer(f, t, s);
+                cout << "Transfer finalizat!\n";
+            }
+            else if (opt == 8)
+            {
+                int id;
+                cout << "ID cont: ";
+                cin >> id;
+                bank.getAcc(id)->printHistory();
+            }
+        }
+        catch (exception &e)
+        {
+            cout << "[EROARE] " << e.what() << "\n";
+        }
     }
 
     return 0;
